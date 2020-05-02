9c9ce33c31f4ad2a6ce9aa890e7e2e07a6f321f4
tkr00st <tkr00st@gmail.com>
2020-05-02 15:36:14 +0900

    グローバルメニューのトグルを実装

--- common-script.js
+++ common-script.js
@@ -9,11 +9,22 @@ const utility = {
   }
 };
 
+const topPage = {
+  globalNavFirstClick: _e => {
+    _e.currentTarget.setAttribute("firstClicked", "firstClicked");
+  }
+};
+
 const init = () => {
   const globalNav = document.querySelector(".global-nav");
-  globalNav
-    .querySelector(".global-nav__toggle-button")
-    .addEventListener("click", utility.toggleClick);
+  const partsToggleMunu = document.querySelectorAll(".parts-toggle-munu");
+  globalNav.addEventListener("click", utility.toggleClick);
+  globalNav.addEventListener("click", topPage.globalNavFirstClick);
+  partsToggleMunu.forEach(_e => {
+    _e.addEventListener("click", utility.toggleClick);
+  });
 };
 
 init();
+
+console.log("test");
