c7ed8c1e0439f661b0ae791ff9f8811822c44927
kahohorimoto <53282480+kahohorimoto@users.noreply.github.com>
2020-05-09 18:13:44 +0900

    200509_contact修正

--- contact/contact-script.js
+++ contact/contact-script.js
@@ -11,24 +11,19 @@ $(function() {
       $("#contact_tpl_button_1st, .button").text("非表示");
     }
   });
+
+  //コピーの挙動
+  //$("#contact_tpl_button_1st").click(function() {
+  //  document.getElementById( "#copy_target li" ).value = "list" ;
+  //  $("#copy_area").append('<textarea id="copy_textarea"></textarea>')
+  //  document.getElementById( "#copy_textarea" ).value = "target" ;
+  //  list.each ->
+  //    target.append($(@).text() + '\n')
+  //  target.select()
+  //  document.execCommand("Copy")
+  //  target.remove()
+  //  alert("ブレスト結果をコピーしました！")
+  //https://qiita.com/at-946/items/16902fc9e26926d6ff4f
+
+  //下記は消さない
 });
-
-//コピーの挙動
-// コピーボタン
-//  const btn_copy = document.querySelector("#contact_tpl_button_2nd");
-// コピー対象となるテキスト
-//  const copy_target = document.querySelector("#contact_tpl > li");
-
-// コピーボタンのクリックイベント
-//  btn_copy.addEventListener("click", () => {
-//    copyText(copy_target);
-// });
-
-//  const copyText = target => {
-// テキストの選択
-//    document.getSelection().selectAllChildren(target);
-// 選択範囲のコピー
-//    document.execCommand("copy");
-// テキスト選択の解除
-//    document.getSelection().empty(target);
-//  };
