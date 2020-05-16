1c8127e8d028c74d2524fb62729fa04d635dde94
tkr00st <tkr00st@gmail.com>
2020-05-16 18:39:30 +0900

    ヘッダー、スクロールによる挙動

--- common-script.js
+++ common-script.js
@@ -21,10 +21,40 @@ const utility = {
   }
 };
 
+const commonWork = {
+  toggleGlobalHeaderWithScroll: _globalHeader => {
+    let scrollFlag = false;
+    const threshold = _globalHeader.clientHeight;
+    const toggleScrolledAttr = _e => {
+      if (!scrollFlag) {
+        window.requestAnimationFrame(() => {
+          scrollFlag = false;
+          const scrolledName = "scrolled";
+          if (window.scrollY > threshold) {
+            _globalHeader.setAttribute(scrolledName, scrolledName);
+            console.log("多い: " + window.scrollY);
+          } else {
+            if (_globalHeader.hasAttribute(scrolledName)) {
+              _globalHeader.removeAttribute(scrolledName);
+            }
+            console.log("少ない: " + window.scrollY);
+          }
+        });
+        scrollFlag = true;
+      }
+    };
+    window.addEventListener("scroll", toggleScrolledAttr, {
+      passive: true
+    });
+  }
+};
+
 const init = () => {
   const globalNav = document.querySelector(".global-nav");
+  const globalHeader = document.querySelector(".global-header");
   globalNav.addEventListener("click", utility.toggleClickedAttr);
   globalNav.addEventListener("click", utility.addfirstClickedAttr);
+  // commonWork.toggleGlobalHeaderWithScroll(globalHeader);
 };
 
 init();
